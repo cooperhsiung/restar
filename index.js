ae160e8e8faa16abc3683e05305d1e334a56d7e9
Cooper Hsiung <cooperhsiung@gmail.com>
2018-08-25 18:18:59 +0800

    imp

--- index.js
+++ index.js
@@ -86,30 +86,3 @@ class Restar {
 });
 
 module.exports = Restar;
-
-
-let app = new Restar();
-
-app.use((req, res) => {
-  res.setHeader('X-Powered-By', 'Restar');
-});
-
-app.get('/test', () => ({ name: 'restar' }));
-
-app.post('/test', ({ param: { name } }) => {
-  return name || null;
-});
-
-app.get(
-  '/sleep',
-  async () => {
-    await sleep();
-  },
-  () => 'sleep 1s'
-);
-
-function sleep(delay = 1000) {
-  return new Promise(resolve => setTimeout(resolve, delay));
-}
-
-app.listen(3000);
